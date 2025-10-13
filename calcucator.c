17e225a979e9b89eb4e2d0c3eb516c503df0ec40
Xodros <nikosalexandris07@gmail.com>
2025-10-13 13:34:52 +0300

    V1

--- calcucator.c
+++ calcucator.c
@@ -3,5 +3,26 @@
 #include <stdio.h>
 
 int main() {
-	scanf("%c",&praxh)
-	if 
+	double a,b;
+	char praxh;
+	printf("Δώσε τον πρώτο αριθμό: ");
+	scanf("%lf",&a);
+	printf("Δώσε τον δεύτερο αριθμό: ");
+	scanf("%lf",&b);
+	printf("Δώσε την πράξη (+, -, *, /): ");
+	scanf(" %c",&praxh);
+	if (praxh=='+') {
+		printf("%lf \n",a+b);
+	} else if (praxh=='-') {
+		printf("%lf \n",a-b);
+	} else if (praxh=='*') {
+		printf("%lf \n",a*b);
+	} else {
+		if (b!=0) {
+			printf("%lf \n",a/b);
+		} else {
+			printf("b=0!\n");
+		}
+	}
+	return 0;
+}
